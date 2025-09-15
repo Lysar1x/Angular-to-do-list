98ae8c8ddcfc3a9512f94ddf0878a5410f7cc63a
Paco <lysar1x950@gmail.com>
2025-09-14 20:45:21 -0600

    Taskist configurated

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { TaskList } from './task-list/task-list';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, TaskList],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
